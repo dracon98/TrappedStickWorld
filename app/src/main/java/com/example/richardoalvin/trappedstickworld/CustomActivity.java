27036b9954e416772acfe0ccb22f2c7f813c6498
Richardo Alvin <richardo.alvin@yahoo.com>
2018-03-24 23:13:34 +1100

    customized and randomized stats in custom activity with preparation to the MainActivity

--- app/src/main/java/com/example/richardoalvin/trappedstickworld/CustomActivity.java
+++ app/src/main/java/com/example/richardoalvin/trappedstickworld/CustomActivity.java
@@ -7,6 +7,9 @@ import android.widget.ImageButton;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
+
+import java.util.Random;
+
 /**
  * Created by Richardo Alvin on 3/24/2018.
  */
@@ -14,9 +17,12 @@ import android.content.Intent;
 public class CustomActivity extends AppCompatActivity {
 
     private ImageButton back;
+    private Button Start;
     private EditText Agi;
     private EditText Intel;
     private EditText Str;
+    private ImageButton roll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +40,21 @@ public class CustomActivity extends AppCompatActivity {
         Agi.setEnabled(false);
         Str.setEnabled(false);
         Intel.setEnabled(false);
+
+        roll = (ImageButton) findViewById(R.id.rollId);
+        roll.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                RandomPoint();
+            }
+        });
+        Start = (Button) findViewById(R.id.startID);
+        Start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                MoveToMain();
+            }
+        });
     }
         public void MoveToMenu(){
             try {
@@ -42,5 +63,25 @@ public class CustomActivity extends AppCompatActivity {
             } catch(Exception e) {
                 e.printStackTrace();
             }
+        }
+        public void MoveToMain(){
+            try {
+                Intent k = new Intent(this, MainActivity.class);
+                startActivity(k);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        public  void RandomPoint(){
+            Random r = new Random();
+            int i = r.nextInt(10) + 1;
+            int o = r.nextInt(10) + 1;
+            int in = r.nextInt(10) + 1;
+            int j = 21 - o - i;
+            int n = 21 - in - i;
+            int m = 21 - in - o;
+            Agi.setText(Integer.toString(j));
+            Str.setText(Integer.toString(n));
+            Intel.setText(Integer.toString(m));
         }
 }
